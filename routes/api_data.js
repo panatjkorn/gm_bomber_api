3e95e1de0a8a79e9f842c9370eaa0d14b46e501b
Panatjkorn <forworkbomb001@gmail.com>
2022-07-13 14:12:36 +0700

    test result lotto hanoi

--- routes/api_data.js
+++ routes/api_data.js
@@ -2,9 +2,12 @@ const express = require("express")
 const router = express.Router()
 
 const {
-    getPanelPrice
+    getPanelPrice,
+    ResultLottoHanoi
 } = require("../controllers/DataController")
 
 router.get("/get_panel_price",getPanelPrice)
+
+router.post("/result/lottos/hanoi",ResultLottoHanoi)
 
 module.exports = router
