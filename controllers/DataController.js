b47754ffaba60d1fb7d28246f302d6a79f8572a3
Panatjkorn <forworkbomb001@gmail.com>
2022-07-14 11:33:12 +0700

    test result hanoi

--- controllers/DataController.js
+++ controllers/DataController.js
@@ -41,43 +41,45 @@ exports.ResultLottoHanoi = async (req, res) => {
     try {
 
         let response = null;
-
-            const url = `https://www.ruay.at/%e0%b8%95%e0%b8%a3%e0%b8%a7%e0%b8%88%e0%b8%ab%e0%b8%a7%e0%b8%a2%e0%b8%ae%e0%b8%b2%e0%b8%99%e0%b8%ad%e0%b8%a2/`
+            const url = `https://www.ruay.at`
+            // const url = `https://www.ruay.at/%e0%b8%95%e0%b8%a3%e0%b8%a7%e0%b8%88%e0%b8%ab%e0%b8%a7%e0%b8%a2%e0%b8%ae%e0%b8%b2%e0%b8%99%e0%b8%ad%e0%b8%a2/`
 
             const lottoHanoi = await axios.get(url)
             console.log('lottoHanoi',lottoHanoi);
-
-            const $ = cheerio.load(lottoHanoi.data)
-
-            const date = $('#page > #content > div > section:nth-child(3) > div > div:nth-child(1) > div > div:nth-child(1) > div > div > div > section > div > div > div > div > div > table > tbody > tr:nth-child(1) > td > span > span:nth-child(2)').text().trim()
-            const fitrst_prize = $('#page > #content > div > section:nth-child(3) > div > div:nth-child(1) > div > div:nth-child(1) > div > div > div > section > div > div > div > div > div > table > tbody > tr:nth-child(2) > td:nth-child(2) > span > span > b').text().trim()
-            const last_two_prize = $('#page > #content > div > section:nth-child(3) > div > div:nth-child(1) > div > div:nth-child(1) > div > div > div > section > div > div > div > div > div > table > tbody > tr:nth-child(4) > td:nth-child(2) > span > span > b').text().trim()
-
-            response = {
-                "date": `${date.split(" ")[1]} ${date.split(" ")[2]} ${date.split(" ")[3]}`,
-                "first_prize": fitrst_prize,
-                "last_two_prize": last_two_prize
-            }
-
-            // console.log('response',response);
-
-            if (!fitrst_prize || fitrst_prize == '' || !last_two_prize || last_two_prize == '') {
-                return res.status(400).json({
-                    success: false,
-                    errors: `ผลยังไม่ออก`
-                });
-            }
-
             return res.status(200).json({
                 success: true,
-                data: response
+                data: lottoHanoi
             });
+            // const $ = cheerio.load(lottoHanoi.data)
+
+            // const date = $('#page > #content > div > section:nth-child(3) > div > div:nth-child(1) > div > div:nth-child(1) > div > div > div > section > div > div > div > div > div > table > tbody > tr:nth-child(1) > td > span > span:nth-child(2)').text().trim()
+            // const fitrst_prize = $('#page > #content > div > section:nth-child(3) > div > div:nth-child(1) > div > div:nth-child(1) > div > div > div > section > div > div > div > div > div > table > tbody > tr:nth-child(2) > td:nth-child(2) > span > span > b').text().trim()
+            // const last_two_prize = $('#page > #content > div > section:nth-child(3) > div > div:nth-child(1) > div > div:nth-child(1) > div > div > div > section > div > div > div > div > div > table > tbody > tr:nth-child(4) > td:nth-child(2) > span > span > b').text().trim()
+
+            // response = {
+            //     "date": `${date.split(" ")[1]} ${date.split(" ")[2]} ${date.split(" ")[3]}`,
+            //     "first_prize": fitrst_prize,
+            //     "last_two_prize": last_two_prize
+            // }
+
+            // // console.log('response',response);
+
+            // if (!fitrst_prize || fitrst_prize == '' || !last_two_prize || last_two_prize == '') {
+            //     return res.status(400).json({
+            //         success: false,
+            //         errors: `ผลยังไม่ออก`
+            //     });
+            // }
+
+            // return res.status(200).json({
+            //     success: true,
+            //     data: response
+            // });
 
     } catch (error) {
-        console.log('error',error);
         return res.status(400).json({
             success: false,
-            errors: error.message
+            errors: error
         });
     }
 
